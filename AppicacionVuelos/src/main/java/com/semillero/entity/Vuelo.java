8e205e9719560a48e662c05246f80a7ee7f234f3
Juliana Marin <juliana.marin04@gmail.com>
2020-04-06 20:13:06 -0500

    Creacion de entidad Vuelo

---
+++ AppicacionVuelos/src/main/java/com/semillero/entity/Vuelo.java
@@ -0,0 +1,82 @@
+package com.semillero.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+public class Vuelo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator= "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	private int idVuelo;
+	
+	@Column
+	private String numeroVuelo;
+	
+	@Column
+	private String modeloAvion;
+	
+	@Column
+	private int capacidad;
+
+	
+	public Vuelo(String numeroVuelo, String modeloAvion, int capacidad) {
+		this.modeloAvion = modeloAvion;
+		this.numeroVuelo = numeroVuelo;
+		this.capacidad = capacidad;
+	}
+	
+
+	public int getIdVuelo() {
+		return idVuelo;
+	}
+
+	public void setIdVuelo(int idVuelo) {
+		this.idVuelo = idVuelo;
+	}
+
+	public String getNumeroVuelo() {
+		return numeroVuelo;
+	}
+
+	public void setNumeroVuelo(String numeroVuelo) {
+		this.numeroVuelo = numeroVuelo;
+	}
+
+	public String getModeloAvion() {
+		return modeloAvion;
+	}
+
+	public void setModeloAvion(String modeloAvion) {
+		this.modeloAvion = modeloAvion;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Vuelo [idVuelo=" + idVuelo + ", numeroVuelo=" + numeroVuelo + ", modeloAvion=" + modeloAvion
+				+ ", capacidad=" + capacidad + "]";
+	}
+	
+	
+}
