f171bb3b21188595b7f671c1fa57ca3f7eaf0a36
Juliana Marin <juliana.marin04@gmail.com>
2020-04-06 21:06:00 -0500

    Archivo html

---
+++ AppicacionVuelos/src/main/java/com/semillero/controller/ClienteController.java
@@ -0,0 +1,13 @@
+package com.semillero.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClienteController {
+
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+}
