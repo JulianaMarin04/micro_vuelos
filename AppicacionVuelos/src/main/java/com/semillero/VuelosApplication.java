7657b2c1564bc1985b3ef34ecb3394b3d707e93e
Juliana Marin <juliana.marin04@gmail.com>
2020-04-07 16:49:59 -0500

    Controllers

--- AppicacionVuelos/src/main/java/com/semillero/VuelosApplication.java
+++ AppicacionVuelos/src/main/java/com/semillero/VuelosApplication.java
@@ -1,13 +1,35 @@
 package com.semillero;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppicacionVuelosApplication {
+import com.semillero.services.ClienteService;
 
+
+
+@SpringBootApplication
+public class VuelosApplication implements CommandLineRunner{
+
+	private static Logger LOG = LoggerFactory.getLogger(VuelosApplication.class);
+	
+	@Autowired
+	private  ClienteService clienteService;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(AppicacionVuelosApplication.class, args);
+		SpringApplication.run(VuelosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Hola mundo");
+		//LOG.info("Hola mundo");
+		
+		
+		
 	}
 
 }
--- AppicacionVuelos/src/main/java/com/semillero/controller/ClienteController.java
+++ AppicacionVuelos/src/main/java/com/semillero/controller/ClienteController.java
@@ -1,13 +1,23 @@
 package com.semillero.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.semillero.repository.ClienteRepository;
 
 @Controller
 public class ClienteController {
 
-	@GetMapping
-	public String index() {
+	@Autowired
+	private ClienteRepository repositorioCliente;
+	
+	
+	@GetMapping("/home")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="Word") String name, Model model) {
+		model.addAttribute("name",name);
 		return "index";
 	}
 }
