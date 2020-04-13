8fa6d7f0a16906af3ef94dc8c65794f4c55f486c
Juliana Marin <juliana.marin04@gmail.com>
2020-04-12 21:08:04 -0500

    Crossorigins

--- AppicacionVuelos/src/main/java/com/semillero/controller/ClienteController.java
+++ AppicacionVuelos/src/main/java/com/semillero/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.semillero.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.semillero.entity.Cliente;
 import com.semillero.services.ClienteServiceImpl;
-
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/Home")
 public class ClienteController {
